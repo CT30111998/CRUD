41706fd62f02229f770b5cedea7e033529801076
Chirag30111998 <chirag.t@peerbits.com>
2022-03-28 16:00:53 +0530

    first commit

---
+++ ui/crud1/src/app/employee/show-emp/show-emp.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-show-emp',
+  templateUrl: './show-emp.component.html',
+  styleUrls: ['./show-emp.component.css']
+})
+export class ShowEmpComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  EmployeeList:any=[];
+
+  ModalTitle:any;
+  ActivateAddEditEmpComp:boolean=false;
+  emp:any;
+
+  ngOnInit(): void {
+    this.refreshEmpList();
+  }
+
+  //for gate data from databases
+  refreshEmpList(){
+    this.service.getEmpList().subscribe(data=>{
+      this.EmployeeList=data;
+    });
+  }
+
+  //For add new fild
+  addClick(){
+    this.emp={
+      EmployeeId:0,
+      EmployeeName:"",
+      EmailId:"",
+      Department:"",
+      DateOfJoining:"",
+      PhotoFileName:"",
+    }
+    this.ModalTitle="Add Employees";
+    this.ActivateAddEditEmpComp=true;
+  }
+
+  //For Add in current Fild
+  editClick(item:any){
+    console.log(item);
+    this.emp=item;
+    this.ModalTitle="Edit Employee";
+    this.ActivateAddEditEmpComp=true;
+  }
+
+  //for delete
+  deleteClick(item:any){
+    if(confirm('Are you sure??')){
+      this.service.deleteEmployee(item.EmployeeId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshEmpList();
+      })
+    }
+  }
+
+  //for Time
+  closeClick(){
+    this.ActivateAddEditEmpComp=false;
+    this.refreshEmpList();
+  }
+
+
+
+
+}
