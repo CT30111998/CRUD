5ef7a1f4d120f2bac084e9dbc89300de85f60e3f
Chirag30111998 <chirag.t@peerbits.com>
2022-03-29 19:03:51 +0530

    fromating

--- DjangoAPI/EmployeeApp/urls.py
+++ DjangoAPI/EmployeeApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
    re_path(r'^department/$', views.departmentApi),
    re_path(r'^department/([0-9]+)$', views.departmentApi),
@@ -13,5 +14,5 @@ urlpatterns = [
    re_path(r'^employee/([0-9]+)$', views.employeeApi),
     
    re_path(r'^SaveFile$', views.SaveFile)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
